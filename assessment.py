be85fba8d4ab18c63e52cc8b8e2971597bf1cb56
unknown <milan563025@gmail.com>
2023-04-29 07:19:06 +0530

    Assessment-01 File

---
+++ assessment.py
@@ -0,0 +1,13 @@
+from flask import flask
+app= Flask(__name__)
+@app.route('/')
+def table():
+    print("This is an assessment.")
+    for (i==2 && j>=1 && j<=10):
+        int c==i*j
+        print(c)
+        print(" ")
+    
+        j++
+
+
