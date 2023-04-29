72594ad0dd0b71ab0799fd43d3490166a301a867
unknown <milan563025@gmail.com>
2023-04-29 11:30:25 +0530

    First Assessment

--- assessment.py
+++ assessment.py
@@ -2,12 +2,6 @@ from flask import flask
 app= Flask(__name__)
 @app.route('/')
 def table():
-    print("This is an assessment.")
-    for (i==2 && j>=1 && j<=10):
-        int c==i*j
-        print(c)
-        print(" ")
-    
-        j++
+    return 'This is an assessment.'
 
 
